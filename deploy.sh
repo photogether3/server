c2e607814b0a299faf389d72c7ce6b8495372a70
dev.goraebap <dev.goraebap@gmail.com>
2025-01-19 20:28:07 +0900

    Chore: update file

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ VERSION=$(node -p "require('./package.json').version")
 npm run build
 
 # # STEP: 도커 이미지 빌드
-docker build -f ./docker/Dockerfile -t gcr.io/alexandrialib-754c4/api:${VERSION} .
+docker build -t gcr.io/alexandrialib-754c4/api:${VERSION} .
 
 # STEP: gcr.io 도커 허브에 이미지 푸쉬
 docker push gcr.io/alexandrialib-754c4/api:${VERSION}
