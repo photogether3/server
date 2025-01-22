f787f070e85da3cf172b650a7a19949be97aacb3
dev.goraebap <dev.goraebap@gmail.com>
2025-01-22 15:25:35 +0900

    Feat: update [create collection] features

--- src/app/collection/collection.facade.ts
+++ src/app/collection/collection.facade.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
+import { FavoriteCoreService } from '../favorite/public-api';
+
 import { CreateCollectionDTO } from './collection.dto';
 import { CollectionCoreService } from './collection-core/collection-core.service';
 import { CreateCollectionParam } from './collection-core/collection.model';
@@ -8,11 +10,14 @@ import { CreateCollectionParam } from './collection-core/collection.model';
 export class CollectionFacade {
 
     constructor(
+        private readonly favoriteCoreService: FavoriteCoreService,
         private readonly collectionCoreService: CollectionCoreService,
     ) {
     }
 
     async create(userId: string, dto: CreateCollectionDTO) {
+        await this.favoriteCoreService.getFavoriteOrThrow(dto.favoriteId);
+
         const param: CreateCollectionParam = { userId, ...dto };
         await this.collectionCoreService.create(param);
     }
--- src/app/collection/collection.module.ts
+++ src/app/collection/collection.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { FavoriteCoreModule } from '../favorite/public-api';
+
 import { CollectionController } from './collection.controller';
 import { CollectionFacade } from './collection.facade';
 import { CollectionCoreModule } from './collection-core/collection-core.module';
 
 @Module({
-    imports: [CollectionCoreModule],
+    imports: [CollectionCoreModule, FavoriteCoreModule],
     controllers: [CollectionController],
     providers: [CollectionFacade],
 })
