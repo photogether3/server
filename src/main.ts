fe493109882755111f2058e88604c4a0725183ac
dev.goraebap <dev.goraebap@gmail.com>
2025-01-31 02:08:31 +0900

    Chore: update version [0.8.3]

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
         .setTitle('사진첩 덜어내기')
         .setDescription('API 문서')
         .addBearerAuth()
-        .setVersion('0.8.2')
+        .setVersion('0.8.3')
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
